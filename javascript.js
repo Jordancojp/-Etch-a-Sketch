d03228f011bd9be55767347cf530e43f3d1dc07a
Jordancojp <117468147+Jordancojp@users.noreply.github.com>
2024-10-27 21:56:06 +0800

    added visual representation to currently selected pallet node.

--- javascript.js
+++ javascript.js
@@ -6,6 +6,8 @@ newDocBtn.onclick = () => generateGrid(newDocInput.value);
 const colorPalletGrid = document.querySelector('#Pallet');
 var selectedColor = '#000000';
 const colorPallet = ['#2e222f','#3e3546', '#625565', '#966c6c', '#ab947a', '#694f62', '#7f708a', '#9babb2', '#c7dcd0', '#ffffff', '#6e2727', '#b33831', '#ea4f36', '#f57d4a', '#ae2334', '#e83b3b', '#fb6b1d', '#f79617', '#f9c22b', '#7a3045', '#9e4539', '#cd683d', '#e6904e', '#fbb954', '#4c3e24', '#676633', '#a2a947', '#d5e04b', '#fbff86', '#165a4c', '#239063', '#1ebc73', '#91db69', '#cddf6c', '#313638', '#374e4a', '#547e64', '#92a984', '#b2ba90', '#0b5e65', '#0b8a8f', '#0eaf9b', '#30e1b9', '#8ff8e2', '#323353', '#484a77', '#4d65b4', '#4d9be6', '#8fd3ff', '#45293f', '#6b3e75', '#905ea9', '#a884f3', '#eaaded', '#753c54', '#a24b6f', '#cf657f', '#ed8099', '#831c5d', '#c32454', '#f04f78', '#f68181', '#fca790', '#fdcbb0' ];
+var activePalletNode;
+
 
 generateGrid(16);
 generateColorPallet();
@@ -37,6 +39,16 @@ function generateColorPallet() {
         gridNode.classList.add('GridNode');
         colorPalletGrid.appendChild(gridNode);
         gridNode.style.backgroundColor = element;
-        gridNode.onclick = () => selectedColor = element;
+        gridNode.onclick = () => changeSelectedColor(element, gridNode);
     });
+}
+
+function changeSelectedColor(color, node)
+{
+    selectedColor = color;
+    if(activePalletNode != null)
+        activePalletNode.style.boxShadow = '';
+
+    activePalletNode = node;
+    activePalletNode.style.boxShadow ='0 0 8px inset rgb(0, 217, 255)';
 }
